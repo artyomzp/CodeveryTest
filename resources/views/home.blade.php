e85b5404dc22014326a47bd9c29ca325ad147e3b
Artyom <serdyukartyom.zp@gmail.com>
2018-08-16 07:35:06 +0300

    add message and comments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
                     {{ session('status') }}
                 </div>
             @endif
             <h1>Hello {{ Auth::user()->name }}</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col col-md-12">
+            
         </div>
     </div>
 </div>
